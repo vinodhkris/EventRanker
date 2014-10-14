830bc0fe441e458baf495e27c7e0aaa179666769
codenut27 <krishnan.vinodh@gmail.com>
2014-10-13 21:38:37 -0400

    Updated Scraper

--- Reuters/newYorkTimesScraper.py
+++ Reuters/newYorkTimesScraper.py
@@ -4,7 +4,10 @@ import utils
 import json
 from bs4 import BeautifulSoup
 from collections import defaultdict
-
+import random
+import test
+import pickle
+import stripper
 # helpful function for processing keywords, mostly    
 def getMultiples(items, key):
     values_list = ""
@@ -19,47 +22,62 @@ def getMultiples(items, key):
     return values_list
 
 def parse_nyt():
-	
 	url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=stock+market+crash+&begin_date=20070101&end_date=20090101&api-key=318a69b2af97848f66071cb4c1fdc831:15:69992102" 
 	response = urlopen(url).read()
 	response = json.loads(response)
-	
 	print "Got response from nytimes"
-
 	articleContent = []
 	i = 0
 	page = 1
 	hits = response["response"]["meta"]["hits"]
-	while i<10 and page<(hits/10):
+	while i<51 and page<(hits/10):
 		print 'Getting response for page',page
 		url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=stock+market+crash+&begin_date=20070101&end_date=20090101&page="+str(page)+"&api-key=318a69b2af97848f66071cb4c1fdc831:15:69992102" 
 		response = urlopen(url).read()
 		response = json.loads(response)
 		for article in response["response"]["docs"]:
-			if article["word_count"]>200 and article["lead_paragraph"]!=None:
-				articleContent.append({})
-				articleContent[i]["abstract"] = article["abstract"]
-				articleContent[i]["pub_date"] = article["pub_date"]
-				articleContent[i]["headline"] = article["headline"]["main"]
-				articleContent[i]["keywords"] = article["keywords"]
-				articleContent[i]["lead_paragraph"] = article["lead_paragraph"]
-				articleContent[i]["web_url"] = article["web_url"]
-				articleContent[i]["id"] = article["_id"]
-				articleContent[i]["word_count"] = article["word_count"]
-				keywords = ""
-				keywords = getMultiples(article["keywords"],"value")
-				# should probably pull these if/else checks into a module
-				variables = [article["pub_date"], keywords, str(article["headline"]["main"]) if "main" in article["headline"].keys() else "", str(article["source"]) if "source" in article.keys() else "", str(article["document_type"]) if "document_type" in article.keys() else "", article["web_url"] if "web_url" in article.keys() else "",str(article["news_desk"]) if "news_desk" in article.keys() else "",str(article["section_name"]) if "section_name" in article.keys() else "",str(article["lead_paragraph"]).replace("\n","") if "lead_paragraph" in article.keys() else ""]
-				line = "\t".join(variables)
-				articleContent[i]["text"] = line
-				i+=1
-				print 'Extracted',i,article["word_count"]
-				if i>10:
-					break
+			if random.randint(0,3) == 3: 								#1/3 probability
+				print article["web_url"]
+				soup1 = BeautifulSoup(test.getData(article["web_url"]))
+				soup = soup1.findAll("p",{"itemprop": "articleBody"})
+				if soup == None or len(soup) == 0:
+					soup = soup1.find("div", {"id": "articleBody"})
+					if soup!=None:
+						soup = soup.findAll("p")
+				if soup == None or len(soup)==0:
+					soup = soup1.find("div", {"class": "articleBody"}) 
+					if soup!=None:
+						soup = soup.findAll("p")
+				if soup!=None or len(soup)==0:
+					if article["word_count"]>200 and article["lead_paragraph"]!=None:
+						articleContent.append({})
+						articleContent[i]["abstract"] = article["abstract"]
+						articleContent[i]["pub_date"] = article["pub_date"]
+						articleContent[i]["headline"] = article["headline"]["main"]
+						articleContent[i]["keywords"] = article["keywords"]
+						articleContent[i]["lead_paragraph"] = article["lead_paragraph"]
+						articleContent[i]["web_url"] = article["web_url"]
+						articleContent[i]["id"] = article["_id"]
+						articleContent[i]["word_count"] = article["word_count"]
+						keywords = ""
+						keywords = getMultiples(article["keywords"],"value")
+						# should probably pull these if/else checks into a module
+						#	variables = [article["pub_date"], keywords, str(article["headline"]["main"]) if "main" in article["headline"].keys() else "", str(article["source"]) if "source" in article.keys() else "", str(article["document_type"]) if "document_type" in article.keys() else "", article["web_url"] if "web_url" in article.keys() else "",str(article["news_desk"]) if "news_desk" in article.keys() else "",str(article["section_name"]) if "section_name" in article.keys() else "",str(article["lead_paragraph"]).replace("\n","") if "lead_paragraph" in article.keys() else ""]
+						#	line = "\t".join(variables)
+						#	articleContent[i]["text"] = line
+						sent = ""
+						if type(soup) is not str:
+							sent = " ".join([str(word) for word in soup])
+						else:
+							sent = soup
+						articleContent[i]["text"] = stripper.strip(sent)
+						i+=1
+						print 'Extracted',i,article["pub_date"]
+						if i>51:
+							break
 		page+=1
 
-	print "Articles Extracted",i
-	
+	print "Articles Extracted",i	
 	return articleContent
 
 def writePickle(struct, filename):
@@ -68,5 +86,5 @@ def writePickle(struct, filename):
 	file1.close()
 
 articleContent = parse_nyt()
-writePickle(articleContent,"articleContent.txt")
+writePickle(articleContent,"stockmarketarticleContent.txt")
 
--- Reuters/test.py
+++ Reuters/test.py
@@ -5,9 +5,16 @@ from bs4 import BeautifulSoup
 response = urllib2.urlopen('http://python.org/')
 html = response.read()
 '''
-url = "http://www.nytimes.com/2008/12/28/books/review/Gross-t.html"
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-request = urllib2.Request(url)
-response = opener.open(request)
-print response.read()
+def getData(url):
+	html = ""
+	try:
+		response = urllib2.urlopen(url)
+		html = response.read()
+	except:
+		cj = cookielib.CookieJar()
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+		request = urllib2.Request(url)
+		response = opener.open(request)
+		html = response.read()
+
+	return html
